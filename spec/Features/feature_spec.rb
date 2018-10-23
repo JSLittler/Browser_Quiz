ceab3ef01bb1d1f467206bdb32c39185083b0c46
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-23 11:56:37 +0100

    23-10-2018 @12pm: Tests for question infrastructure written and passed.

--- spec/Features/feature_spec.rb
+++ spec/Features/feature_spec.rb
@@ -2,7 +2,20 @@ feature 'Browser_Quiz' do
 
   scenario 'seeing multiple choice questions' do
     sign_in
-    expect(page).to have_content 'Here is a question'
+    expect(page).to have_content 'Hi Ned Stark. Click on the correct answer:'
+  end
+
+  scenario 'seeing a question' do
+    sign_in
+    expect(page).to have_content 'Question'
+  end
+
+  scenario 'seeing multiple choice answers' do
+    sign_in
+    expect(page).to have_content 'A:'
+    expect(page).to have_content 'B:'
+    expect(page).to have_content 'C:'
+    expect(page).to have_content 'D:'
   end
 
 end
