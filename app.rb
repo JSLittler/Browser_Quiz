772280c6d37a68eef9f65ad44a985f0417a4de2a
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-24 21:42:54 +0100

    24-10-2018 @10pm: Website working but crashing intermittently and not holding on to a name. Feature tests for selecting answers removed prior to refactor.

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra/base'
-require './lib/player.rb'
+require './lib/player'
+require './lib/question'
 require './lib/model'
 
 class Browser_Quiz < Sinatra::Base
   enable :sessions
+
+  before do
+    @player = Player.instance
+  end
 
   get '/' do
     erb :index
@@ -14,13 +19,31 @@ class Browser_Quiz < Sinatra::Base
     redirect 'questions'
   end
 
-  before do
-    @player = Player.instance
-  end
-
   get '/questions' do
     @player_name = @player.name
+    @question_current = Question.new
+    @question = @question_current.set_question
+    @answer_A = @question_current.set_answer_a
+    @answer_B = @question_current.set_answer_b
+    @answer_C = @question_current.set_answer_c
+    session[:correct_answer] = @question_current.set_correct_answer
     erb :questions
+  end
+
+  post '/answer' do
+    session[:answer] = params[:answer]
+    redirect 'feedback'
+  end
+
+  get '/feedback' do
+    @correct_answer = session[:correct_answer]
+    @player_answer = session[:answer]
+    if @player_answer == @correct_answer
+      @result = "Well done, that is correct."
+    else
+      @result = "Sorry, that answer was wrong."
+    end
+    erb :feedback
   end
 
 end
--- lib/question.rb
+++ lib/question.rb
@@ -1,6 +1,11 @@
 require 'pg'
 
 class Question
+  attr_reader :question_number
+
+  def self.instance
+    @question
+  end
   
   def self.all
     if ENV['ENVIRONMENT'] == 'test'
@@ -11,6 +16,51 @@ class Question
     
     result = connection.exec("SELECT * FROM questions;")
     result.map { |question| question['question'] }
+  end
+
+  def set_question_number
+    @question_number = rand(10)
+  end
+
+  def set_question
+    set_question_number
+    connection = PG.connect(dbname: 'Browser_Quiz')
+    export = connection.exec("SELECT question FROM questions WHERE id = #{@question_number};")
+    obj = export.map{|result| result}
+    objhash = obj[0]
+    @question = objhash["question"]
+  end
+
+  def set_answer_a
+    connection = PG.connect(dbname: 'Browser_Quiz')
+    export = connection.exec("SELECT answer_a FROM questions WHERE id = #{@question_number};")
+    obj = export.map{|result| result}
+    objhash = obj[0]
+    @answer_a = objhash["answer_a"]
+  end
+
+  def set_answer_b
+    connection = PG.connect(dbname: 'Browser_Quiz')
+    export = connection.exec("SELECT answer_b FROM questions WHERE id = #{@question_number};")
+    obj = export.map{|result| result}
+    objhash = obj[0]
+    @answer_b = objhash["answer_b"]
+  end
+
+  def set_answer_c
+    connection = PG.connect(dbname: 'Browser_Quiz')
+    export = connection.exec("SELECT answer_c FROM questions WHERE id = #{@question_number};")
+    obj = export.map{|result| result}
+    objhash = obj[0]
+    @answer_c = objhash["answer_c"]
+  end
+
+  def set_correct_answer
+    connection = PG.connect(dbname: 'Browser_Quiz')
+    export = connection.exec("SELECT correct_answer FROM questions WHERE id = #{@question_number};")
+    obj = export.map{|result| result}
+    objhash = obj[0]
+    @correct_answer = objhash["correct_answer"]
   end
 
 end
